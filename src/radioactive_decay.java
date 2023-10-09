92da09fb092fa9d52b878698fddeb619a13da1c2
Joschliner <joschliner@users.noreply.github.com>
2023-10-09 16:03:38 +0200

    exercise five

---
+++ src/radioactive_decay.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class radioactive_decay {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("After how many years should the percentage of material be calculated: ");
+        int years = scn.nextInt();
+
+        double decay_percentage = Math.exp(-(Math.log(2)/5730) * years) * 100;
+        System.out.println("Percentage left after " + years + " years is: " + decay_percentage);
+    }
+}
