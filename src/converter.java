406f244388edaf2e5acd589810de0a5a52618efe
Joschliner <joschliner@users.noreply.github.com>
2023-10-09 15:24:00 +0200

    exercise two

---
+++ src/converter.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class converter {
+    public static void main(String[] args)  {
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Give temperature in Fahrenheit: ");
+        int inp = scn.nextInt();
+
+        double celsius = (inp-32.0) * (5.0/9.0);
+        System.out.println("Converted to celsius: " + celsius);
+    }
+}
